6a4de772571fca70a8d08c4d4695d255cd552b11
Andy Doan <andy@foundries.io>
2020-02-25 21:44:07 -0600

    Fix CMake logic
    The way I was doing didn't really work, so this is the best I can do for
    now. This gets the tests working.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ cmake-init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+## Just an easy way to get cmake/ninja set up so you don't have to remember
+
+HERE=$(dirname $(readlink -f $0))
+
+if [ "$HERE" = "$(pwd)" ] ; then
+	echo "ERROR: Don't run this from source directory."
+	exit 1
+fi
+
+cmake -GNinja -DBUILD_OSTREE=ON -DBUILD_DOCKERAPP=ON $HERE
--- src/test_lite.sh
+++ src/test_lite.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 build_dir=$(pwd)
 aklite=$1
