9defb7ac4d400e04d7b93c40a21adbdab22518af
Mike Sul <mike.sul@foundries.io>
2020-11-02 12:06:41 +0200

    Move CN fetching to CC file
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/helpers.h
+++ src/helpers.h
@@ -66,7 +66,7 @@ class LiteClient {
   bool isTargetCurrent(const Uptane::Target& target) const;
   bool checkAppsToUpdate(const Uptane::Target& target) const;
   void setAppsNotChecked();
-  std::string getDeviceID() const { return key_manager_->getCN(); }
+  std::string getDeviceID() const;
 
  private:
   FRIEND_TEST(helpers, locking);
