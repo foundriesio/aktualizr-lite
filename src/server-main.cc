663c717ca69b8aafa36857f6d90738f8d7144ae8
Andy Doan <andy@foundries.io>
2021-03-18 12:47:45 -0500

    Add skeleton for aklite server
    This is the skeleton to create an aklite server similar to what dockerd
    does for docker.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

---
+++ src/server-main.cc
@@ -0,0 +1,128 @@
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include "httplib.h"
+#include "libaktualizr/config.h"
+#include "logging/logging.h"
+
+#include "aktualizr-lite/api.h"
+
+namespace bpo = boost::program_options;
+
+static void json_resp(httplib::Response& res, int code, const Json::Value& data) {
+  res.status = code;
+  Json::StreamWriterBuilder builder;
+  auto content = Json::writeString(builder, data);
+  res.set_content(content, "application/json");
+}
+
+class ApiException : public std::exception {
+ public:
+  ApiException(int status, Json::Value resp) : status(status), resp(std::move(resp)) {
+    what_ = "HTTP_" + std::to_string(status) + " : " + this->resp.asString();
+  }
+
+  const char* what() const noexcept override { return what_.c_str(); }
+
+  int status;
+  Json::Value resp;
+
+ private:
+  std::string what_;
+};
+
+static void get_config(const AkliteClient& client, const httplib::Request& req, httplib::Response& res) {
+  (void)req;
+  auto pt = client.GetConfig();
+  std::stringstream jsonss;
+  boost::property_tree::json_parser::write_json(jsonss, pt);
+
+  res.set_content(jsonss.str(), "application/json");
+}
+
+bpo::variables_map parse_options(int argc, char** argv) {
+  bpo::options_description description("aktualizr-lited command line options");
+
+  // clang-format off
+  description.add_options()
+      ("help,h", "print usage")
+      ("loglevel", bpo::value<int>(), "set log level 0-5 (trace, debug, info, warning, error, fatal)")
+      ("config,c", bpo::value<std::vector<boost::filesystem::path> >()->composing(), "configuration file or directory")
+      ("socket-path", bpo::value<boost::filesystem::path>(), "The unix domain socket path to bind to. Default=/var/run/aklite.sock");
+  // clang-format on
+
+  bpo::positional_options_description pos;
+
+  bpo::variables_map vm;
+  std::vector<std::string> unregistered_options;
+  try {
+    bpo::basic_parsed_options<char> parsed_options = bpo::command_line_parser(argc, argv).options(description).run();
+    bpo::store(parsed_options, vm);
+    bpo::notify(vm);
+    if (vm.count("help") != 0) {
+      std::cout << description << "\n";
+      exit(EXIT_FAILURE);
+    }
+  } catch (const bpo::required_option& ex) {
+    // print the error and append the default commandline option description
+    std::cout << ex.what() << std::endl << description;
+    exit(EXIT_FAILURE);
+  } catch (const bpo::error& ex) {
+    LOG_ERROR << "boost command line option error: " << ex.what();
+    exit(EXIT_FAILURE);
+  }
+
+  return vm;
+}
+
+int main(int argc, char** argv) {
+  logger_init(isatty(1) == 1);
+  logger_set_threshold(boost::log::trivial::info);
+
+  bpo::variables_map cli_map = parse_options(argc, argv);
+
+  if (geteuid() != 0) {
+    LOG_ERROR << "Running as non-root!\n";
+  }
+
+  try {
+    auto dirs = AkliteClient::CONFIG_DIRS;
+    if (cli_map.count("config") > 0) {
+      dirs = cli_map["config"].as<std::vector<boost::filesystem::path>>();
+    }
+    AkliteClient client(dirs);
+
+    httplib::Server svr;
+
+    svr.set_logger([](const httplib::Request& req, const httplib::Response& resp) {
+      LOG_INFO << req.method << " " << req.path << " HTTP_" << resp.status << " " << resp.reason;
+    });
+    svr.set_exception_handler([](const auto& req, auto& res, std::exception& e) {
+      (void)req;
+      if (auto* apie = dynamic_cast<ApiException*>(&e)) {
+        json_resp(res, apie->status, apie->resp);
+      } else {
+        Json::Value data;
+        data["error"] = e.what();
+        json_resp(res, 500, data);
+      }
+    });
+
+    svr.Get("/config",
+            [&client](const httplib::Request& req, httplib::Response& res) { get_config(client, req, res); });
+
+    boost::filesystem::path socket_path("/var/run/aklite.sock");
+    if (cli_map.count("socket-path") == 1) {
+      socket_path = cli_map["socket-path"].as<boost::filesystem::path>();
+    }
+    LOG_INFO << "Server started on " << socket_path;
+    if (svr.listen_unix_domain(socket_path.string().c_str())) {
+      return 0;
+    }
+    return 1;
+  } catch (const std::exception& ex) {
+    LOG_ERROR << ex.what();
+  }
+  return EXIT_FAILURE;
+}
---
+++ tests/apiserver_test.cc
@@ -0,0 +1,124 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <boost/filesystem.hpp>
+#include <boost/process.hpp>
+#include <boost/process/env.hpp>
+
+#include "libaktualizr/types.h"
+#include "logging/logging.h"
+#include "test_utils.h"
+#include "uptane_generator/image_repo.h"
+#include "utilities/utils.h"
+
+#include "aktualizr-lite/api.h"
+#include "composeappmanager.h"
+#include "liteclient.h"
+
+#include "docker/composeappengine.h"
+#include "helpers.h"
+#include "ostree/repo.h"
+#include "target.h"
+
+#include "fixtures/liteclienttest.cc"
+
+using ::testing::NiceMock;
+using ::testing::Return;
+
+const char* aklite_bin;
+
+/**
+ * Class MockAppEngine
+ *
+ */
+class MockAppEngine : public AppEngine {
+ public:
+  MockAppEngine(bool default_behaviour = true) {
+    if (!default_behaviour) return;
+
+    ON_CALL(*this, fetch).WillByDefault(Return(true));
+    ON_CALL(*this, verify).WillByDefault(Return(true));
+    ON_CALL(*this, install).WillByDefault(Return(true));
+    ON_CALL(*this, run).WillByDefault(Return(true));
+    ON_CALL(*this, isFetched).WillByDefault(Return(true));
+    ON_CALL(*this, isRunning).WillByDefault(Return(true));
+    ON_CALL(*this, getRunningAppsInfo)
+        .WillByDefault(
+            Return(Utils::parseJSON("{\"app-07\": {\"services\": {\"nginx-07\": {\"hash\": "
+                                    "\"16e36b4ab48cb19c7100a22686f85ffcbdce5694c936bda03cb12a2cce88efcf\"}}}}")));
+  }
+
+ public:
+  MOCK_METHOD(bool, fetch, (const App& app), (override));
+  MOCK_METHOD(bool, verify, (const App& app), (override));
+  MOCK_METHOD(bool, install, (const App& app), (override));
+  MOCK_METHOD(bool, run, (const App& app), (override));
+  MOCK_METHOD(void, remove, (const App& app), (override));
+  MOCK_METHOD(bool, isFetched, (const App& app), (const, override));
+  MOCK_METHOD(bool, isRunning, (const App& app), (const, override));
+  MOCK_METHOD(Json::Value, getRunningAppsInfo, (), (const, override));
+  MOCK_METHOD(void, prune, (const Apps& app), (override));
+};
+
+class ApiServerTest : public fixtures::ClientTest {
+ public:
+  ~ApiServerTest() {
+    if (aklite_ != nullptr) {
+      LOG_INFO << "Stopping aklite server";
+      aklite_->terminate();
+      aklite_->wait_for(std::chrono::seconds(10));
+    }
+  }
+  void startServer() {
+    auto client = createLiteClient(InitialVersion::kOff);
+    client->config.pacman.extra["docker_compose_bin"] = "tests/compose_fake.sh";
+    std::stringstream ss;
+    ss << client->config;
+
+    std::string sota_toml = (test_dir_.Path() / "sota.toml").string();
+    Utils::writeFile(sota_toml, ss.str());
+
+    LOG_INFO << "Starting socket server at " << getSocketPath();
+    aklite_ =
+        std::make_unique<boost::process::child>(aklite_bin, "--config", sota_toml, "--socket-path", getSocketPath());
+  }
+  std::string getSocketPath() { return (test_dir_.Path() / "aklite.sock").string(); }
+
+ protected:
+  std::shared_ptr<LiteClient> createLiteClient(InitialVersion initial_version = InitialVersion::kOn,
+                                               boost::optional<std::vector<std::string>> apps = boost::none) override {
+    app_engine_mock_ = std::make_shared<NiceMock<MockAppEngine>>();
+    return ClientTest::createLiteClient(app_engine_mock_, initial_version, apps);
+  }
+
+  std::shared_ptr<NiceMock<MockAppEngine>>& getAppEngine() { return app_engine_mock_; }
+
+ private:
+  std::shared_ptr<NiceMock<MockAppEngine>> app_engine_mock_;
+  std::unique_ptr<boost::process::child> aklite_;
+};
+
+TEST_F(ApiServerTest, GetConfig) {
+  startServer();
+  HttpClient client(getSocketPath());
+  auto resp = client.get("http://localhost/config", HttpInterface::kNoLimit);
+  ASSERT_TRUE(resp.isOk());
+  auto data = resp.getJson();
+  ASSERT_EQ("true", data["telemetry"]["report_network"].asString());
+}
+
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    std::cerr << argv[0] << " invalid arguments\n";
+    return EXIT_FAILURE;
+  }
+
+  ::testing::InitGoogleTest(&argc, argv);
+  logger_init();
+
+  // options passed as args in CMakeLists.txt
+  fixtures::DeviceGatewayMock::RunCmd = argv[1];
+  fixtures::SysRootFS::CreateCmd = argv[2];
+  aklite_bin = argv[3];
+  return RUN_ALL_TESTS();
+}
