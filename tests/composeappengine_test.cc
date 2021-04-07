3f9cedf5356baf80f88b517e71236cb94dcae032
Mike Sul <mike.sul@foundries.io>
2021-04-07 11:25:11 +0300

    test: turn the common/base test code into fixtures
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- tests/composeappengine_test.cc
+++ tests/composeappengine_test.cc
@@ -10,34 +10,9 @@
 #include "docker/composeappengine.h"
 #include "logging/logging.h"
 
-// test fixtures, just include CC files
-#include "fixtures/composeapp.cc"
-#include "fixtures/dockerdaemon.cc"
-#include "fixtures/dockerregistry.cc"
+#include "fixtures/composeappenginetest.cc"
 
-class ComposeAppEngineeTest : public ::testing::Test {
- protected:
-  ComposeAppEngineeTest() : registry{test_dir_.Path() / "registry"}, daemon_{test_dir_.Path() / "daemon"} {}
-
-  void SetUp() override {
-    auto compose_cmd =
-        boost::filesystem::canonical("tests/docker-compose_fake.py").string() + " " + daemon_.dir().string() + " ";
-    app_engine = std::make_shared<Docker::ComposeAppEngine>(
-        test_dir_.Path() / "compose-apps", compose_cmd, std::make_shared<Docker::DockerClient>(daemon_.getClient()),
-        std::make_shared<Docker::RegistryClient>("https://ota-lite.foundries.io:8443/", registry.getClient(),
-                                                 registry.getClientFactory()));
-  }
-
- protected:
-  TemporaryDirectory test_dir_;
-  fixtures::DockerRegistry registry;
-  fixtures::DockerDaemon daemon_;
-
-  std::shared_ptr<AppEngine> app_engine;
-
- private:
-  std::shared_ptr<HttpInterface> http_client_;
-};
+class ComposeAppEngineeTest : public fixtures::AppEngineTest {};
 
 TEST_F(ComposeAppEngineeTest, Fetch) {
   auto app = registry.addApp(fixtures::ComposeApp::create("app-01"));
