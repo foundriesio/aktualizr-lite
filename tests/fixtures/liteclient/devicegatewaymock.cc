3b12ee2ef7410009b811428d55cf5d894507929a
Andy Doan <andy@foundries.io>
2021-09-08 15:52:01 -0500

    api: Add CheckIn method
    Signed-off-by: Andy Doan <andy@foundries.io>

--- tests/fixtures/liteclient/devicegatewaymock.cc
+++ tests/fixtures/liteclient/devicegatewaymock.cc
@@ -10,8 +10,9 @@ class DeviceGatewayMock {
         url_{certDir.empty() ? "http://localhost:" + port_ : "https://localhost:" + port_},
         req_headers_file_{tuf_.getPath() + "/headers.json"},
         events_file_{tuf_.getPath() + "/events.json"},
+        sota_toml_file_{tuf_.getPath() + "/sota.toml"},
         process_{RunCmd,           "--port",         port_, "--ostree", ostree_.getPath(), "--tuf-repo", tuf_.getPath(),
-    "--headers-file", req_headers_file_, "--events-file", events_file_,"--mtls", certDir}
+    "--headers-file", req_headers_file_, "--events-file", events_file_,"--mtls", certDir, "--sota-toml", sota_toml_file_}
   {
     if (certDir.empty()) {
       TestUtils::waitForServer(url_ + "/");
@@ -34,7 +35,9 @@ class DeviceGatewayMock {
   const std::string& getPort() const { return port_; }
   Json::Value getReqHeaders() const { return Utils::parseJSONFile(req_headers_file_); }
   Json::Value getEvents() const { return Utils::parseJSONFile(events_file_); }
-  Json::Value resetEvents() const { return boost::filesystem::remove(events_file_); }
+  bool resetEvents() const { return boost::filesystem::remove(events_file_); }
+  std::string readSotaToml() const { return Utils::readFile(sota_toml_file_); }
+  bool resetSotaToml() const { return boost::filesystem::remove(sota_toml_file_); }
 
  private:
   const OSTreeRepoMock& ostree_;
@@ -43,6 +46,7 @@ class DeviceGatewayMock {
   const std::string url_;
   const std::string req_headers_file_;
   const std::string events_file_;
+  const std::string sota_toml_file_;
   boost::process::child process_;
 };
 
