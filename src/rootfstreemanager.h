06ef82113ef3e3234374c2d94aa2fedcdec232c9
Mike Sul <mike.sul@foundries.io>
2022-01-10 18:41:34 +0200

    ostreemanager: set TLS certs for remote if specified
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/rootfstreemanager.h
+++ src/rootfstreemanager.h
@@ -7,31 +7,32 @@
 
 class RootfsTreeManager : public OstreeManager {
  public:
-  static constexpr const char *const Name{"ostree"};
+  static constexpr const char* const Name{"ostree"};
   using RequestHeaders = std::unordered_map<std::string, std::string>;
   struct Remote {
     std::string name;
     std::string baseUrl;
     RequestHeaders headers;
+    boost::optional<const KeyManager*> keys;
     bool isRemoteSet{false};
   };
 
-  RootfsTreeManager(const PackageConfig &pconfig, const BootloaderConfig &bconfig,
-                    const std::shared_ptr<INvStorage> &storage, const std::shared_ptr<HttpInterface> &http,
+  RootfsTreeManager(const PackageConfig& pconfig, const BootloaderConfig& bconfig,
+                    const std::shared_ptr<INvStorage>& storage, const std::shared_ptr<HttpInterface>& http,
                     std::shared_ptr<OSTree::Sysroot> sysroot)
       : OstreeManager(pconfig, bconfig, storage, http, new BootloaderLite(bconfig, *storage)),
         sysroot_{std::move(sysroot)},
         http_client_{http},
         gateway_url_{pconfig.ostree_server} {}
 
-  bool fetchTarget(const Uptane::Target &target, Uptane::Fetcher &fetcher, const KeyManager &keys,
-                   const FetcherProgressCb &progress_cb, const api::FlowControlToken *token) override;
+  bool fetchTarget(const Uptane::Target& target, Uptane::Fetcher& fetcher, const KeyManager& keys,
+                   const FetcherProgressCb& progress_cb, const api::FlowControlToken* token) override;
 
  private:
   std::string getCurrentHash() const override { return sysroot_->getCurDeploymentHash(); }
-  void getAdditionalRemotes(std::vector<Remote> &remotes, const std::string &target_name);
+  void getAdditionalRemotes(std::vector<Remote>& remotes, const std::string& target_name);
 
-  void setRemote(const std::string &name, const std::string &url);
+  void setRemote(const std::string& name, const std::string& url, const boost::optional<const KeyManager*>& keys);
 
   std::shared_ptr<OSTree::Sysroot> sysroot_;
   std::shared_ptr<HttpInterface> http_client_;
