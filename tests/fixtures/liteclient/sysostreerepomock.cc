91379e3962753e4fde7050a1ea793d77183a4e32
Mike Sul <mike.sul@foundries.io>
2022-04-25 14:37:39 +0200

    ostree: Detect and return no space error if pull fails
    - Detect whether an ostree pull fails because of insufficient storage space
      by parsing the error log returned from libostree (unfortunately, no error code is
    returned).
    - Return corresponding error code to a caller if such the error
      situation is detected.

--- tests/fixtures/liteclient/sysostreerepomock.cc
+++ tests/fixtures/liteclient/sysostreerepomock.cc
@@ -16,6 +16,10 @@ class SysOSTreeRepoMock {
     executeCmd("ostree", { "admin", "--sysroot=" + path_, "deploy", "--os=" + os_, hash }, "deploy " + hash);
   }
 
+  void setMinFreeSpace(const std::string& size) {
+    executeCmd("ostree", { "--repo=" + path_ + "/ostree/repo", "config", "set", "core.min-free-space-size", size }, "set config " + size);
+  }
+
  private:
   const std::string path_;
   const std::string os_;
