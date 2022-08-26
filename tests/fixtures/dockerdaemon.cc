84a07a65163527c054e3d738abe2634a93e977e0
Mike Sul <mike.sul@foundries.io>
2022-08-26 18:56:08 +0200

    tests: Make tests independent of the real `dockerd`
    - Improve the docker daemon mocking
    - Use the dockerd mock for the compose app test
    - Make the helpers test independent of dockerd
    - Use the httpclient stub for the docker client so the helpers tests do no
      need running docker engine on a host.
    - Set `DOCKER_HOST` to the docker daemon mock for install
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- tests/fixtures/dockerdaemon.cc
+++ tests/fixtures/dockerdaemon.cc
@@ -9,17 +9,27 @@ class DockerDaemon {
   // tests/docker-compose_fake.py fills/populates this file with "running" containers
   static constexpr const char* const ContainersFile{"containers.json"};
  public:
-  DockerDaemon(boost::filesystem::path dir): dir_{std::move(dir)}, port_{TestUtils::getFreePort()}, process_{RunCmd, "--port", port_, "--dir", dir_.string()} {
+  // run two processes, one (http) is needed for the test business logic, another one (unix) to mock docker daemon
+  // since there are spots in the code where the `docker` CLI is invoked directly.
+  DockerDaemon(boost::filesystem::path dir): dir_{std::move(dir)}, port_{TestUtils::getFreePort()},
+               unix_process_{RunCmd, "-u", unix_sock_.PathString(), "--dir", dir_.string()},
+               process_{RunCmd, "--port", port_, "--dir", dir_.string()} {
     // zero containers are running in the beginning
     Utils::writeFile(dir_ / ContainersFile, none_containers_, true);
     TestUtils::waitForServer("http://localhost:" + port_ + "/");
   }
 
+  ~DockerDaemon() {
+    process_.terminate();
+    process_.wait_for(std::chrono::seconds(10));
+    unix_process_.terminate();
+    unix_process_.wait_for(std::chrono::seconds(10));
+  }
+
   const std::string& dataRoot() const { return dir_.string(); }
 
-  std::string getUrl() const {
-    return "http://localhost:" + port_;
-  }
+  std::string getUrl() const { return "http://localhost:" + port_; }
+  std::string getUnixSocket() const { return "unix://" +  unix_sock_.PathString(); }
 
   std::shared_ptr<HttpInterface> getClient() { return std::make_shared<DockerDaemon::HttpClient>(*this); }
   const boost::filesystem::path& dir() const { return dir_; }
@@ -50,9 +60,11 @@ class DockerDaemon {
   };
 
  private:
+  TemporaryFile unix_sock_;
   const std::string none_containers_{"[]"};
   boost::filesystem::path dir_;
   const std::string port_;
+  boost::process::child unix_process_;
   boost::process::child process_;
 };
 
