d9fa3f11b0b81c3440a9732675aca2b3e91feb80
Mike Sul <mike.sul@foundries.io>
2022-09-30 19:23:45 +0200

    bootloader: Remove redundant "rollback" classes
    - Remove redundant "rollback" interfaces and classes since they
      basically implement the same logic as the `Bootloader` class
      implemented within libaktaulizr.
    
    - The only difference/specialization is in implementation of
      `installNotify` which is not used in libaktualizr at all.
    
    - Therefore, this change removes all "rollback" classes and just
      overrides `installNotify` in BootloaderLite.
      The overridden `installNotify` encapsulates the logic that was
      spread across the "rollback" classes.
    
    - Added `bootloader` namespace to encapsulate the bootloader-related
      logic in it, and to align it with the namespacing approach of aklite,
      (each subdirectory implies namespace).
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/bootloader/bootloaderlite.cc
+++ src/bootloader/bootloaderlite.cc
@@ -1,14 +1,101 @@
 #include "bootloaderlite.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+
 #include "bootloader/bootloader.h"
-#include "rollbacks/factory.h"
 #include "storage/invstorage.h"
+#include "utilities/exceptions.h"
 
-BootloaderLite::BootloaderLite(BootloaderConfig config, INvStorage& storage, const std::string& deployment_path)
-    : Bootloader(std::move(config), storage),
-      rollback_(RollbackFactory::makeRollback(config_.rollback_mode, deployment_path)) {}
+namespace bootloader {
 
-void BootloaderLite::setBootOK() const { rollback_->setBootOK(); }
+std::string getVersion(const std::string& deployment_dir, const std::string& ver_file, const std::string& hash);
 
-void BootloaderLite::updateNotify() const { rollback_->updateNotify(); }
+BootloaderLite::BootloaderLite(BootloaderConfig config, INvStorage& storage, OSTree::Sysroot::Ptr sysroot)
+    : Bootloader(std::move(config), storage), sysroot_{std::move(sysroot)} {}
 
-void BootloaderLite::installNotify(const Uptane::Target& target) const { rollback_->installNotify(target); }
+void BootloaderLite::installNotify(const Uptane::Target& target) const {
+  std::string sink;
+  switch (config_.rollback_mode) {
+    case RollbackMode::kBootloaderNone:
+    case RollbackMode::kUbootGeneric:
+      break;
+    case RollbackMode::kUbootMasked: {
+      std::string version = getVersion(sysroot_->deployment_path(), VersionFile, target.sha256Hash());
+      if (version.empty()) {
+        return;
+      }
+      std::string sink;
+      if (Utils::shell("fw_printenv -n bootfirmware_version", &sink) != 0) {
+        LOG_WARNING << "Failed to read bootfirmware_version";
+        return;
+      }
+      LOG_INFO << "Current boot firmware version: " << sink;
+      if (sink != version) {
+        LOG_INFO << "Update firmware to version: " << version;
+        if (Utils::shell("fw_setenv bootupgrade_available 1", &sink) != 0) {
+          LOG_WARNING << "Failed to set bootupgrade_available";
+        }
+      }
+    } break;
+    case RollbackMode::kFioVB: {
+      std::string version = getVersion(sysroot_->deployment_path(), VersionFile, target.sha256Hash());
+      if (version.empty()) {
+        return;
+      }
+      std::string sink;
+      if (Utils::shell("fiovb_printenv bootfirmware_version", &sink) != 0) {
+        LOG_WARNING << "Failed to read bootfirmware_version";
+        sink = std::string();
+      }
+      LOG_INFO << "Current firmware version: " << sink;
+      if (sink != version) {
+        LOG_INFO << "Update firmware to version: " << version;
+        if (Utils::shell("fiovb_setenv bootupgrade_available 1", &sink) != 0) {
+          LOG_WARNING << "Failed to set bootupgrade_available";
+        }
+      }
+    } break;
+    default:
+      throw NotImplementedException();
+  }
+}
+
+std::string getVersion(const std::string& deployment_dir, const std::string& ver_file, const std::string& hash) {
+  try {
+    std::string file;
+    for (auto& p : boost::filesystem::directory_iterator(deployment_dir)) {
+      std::string dir = p.path().string();
+      if (!boost::filesystem::is_directory(dir)) {
+        continue;
+      }
+      if (boost::algorithm::contains(dir, hash)) {
+        file = dir + ver_file;
+        break;
+      }
+    }
+    if (file.empty()) {
+      LOG_WARNING << "Target hash not found";
+      return std::string();
+    }
+
+    LOG_INFO << "Target firmware file: " << file;
+    std::ifstream ifs(file);
+    std::string version((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+    std::string watermark = "bootfirmware_version";
+    std::string::size_type i = version.find(watermark);
+    if (i != std::string::npos) {
+      version.erase(i, watermark.length() + 1);
+      LOG_INFO << "Target firmware version: " << version;
+      return version;
+    } else {
+      LOG_WARNING << "Target firmware version not found";
+      return std::string();
+    }
+  } catch (const std::exception& exc) {
+    LOG_ERROR << "Failed to obtain Target firmware version:  " << exc.what();
+    return "";
+  }
+}
+
+}  // namespace bootloader
