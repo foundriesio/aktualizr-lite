719592d31e701f958968808d8aadaabf272fb59c
Mike Sul <mike.sul@foundries.io>
2023-03-27 17:45:12 +0200

    bootloader: Refactor to re-use set/get env vars
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/bootloader/bootloaderlite.cc
+++ src/bootloader/bootloaderlite.cc
@@ -2,6 +2,7 @@
 
 #include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/format.hpp>
 
 #include "bootloader/bootloader.h"
 #include "storage/invstorage.h"
@@ -18,40 +19,21 @@ void BootloaderLite::installNotify(const Uptane::Target& target) const {
     case RollbackMode::kBootloaderNone:
     case RollbackMode::kUbootGeneric:
       break;
-    case RollbackMode::kUbootMasked: {
-      std::string version = getVersion(sysroot_->deployment_path(), target.sha256Hash());
-      if (version.empty()) {
-        return;
-      }
-      std::string sink;
-      if (Utils::shell("fw_printenv -n bootfirmware_version", &sink) != 0) {
-        LOG_WARNING << "Failed to read bootfirmware_version";
-        return;
-      }
-      LOG_INFO << "Current boot firmware version: " << sink;
-      if (sink != version) {
-        LOG_INFO << "Update firmware to version: " << version;
-        if (Utils::shell("fw_setenv bootupgrade_available 1", &sink) != 0) {
-          LOG_WARNING << "Failed to set bootupgrade_available";
-        }
-      }
-    } break;
+    case RollbackMode::kUbootMasked:
     case RollbackMode::kFioVB: {
-      std::string version = getVersion(sysroot_->deployment_path(), target.sha256Hash());
-      if (version.empty()) {
+      std::string target_version = getVersion(sysroot_->deployment_path(), target.sha256Hash());
+      if (target_version.empty()) {
         return;
       }
-      std::string sink;
-      if (Utils::shell("fiovb_printenv bootfirmware_version", &sink) != 0) {
-        LOG_WARNING << "Failed to read bootfirmware_version";
-        sink = std::string();
+      const auto cur_version{getEnvVar("bootfirmware_version")};
+      if (std::get<1>(cur_version)) {
+        LOG_INFO << "Current bootloader version: " << std::get<0>(cur_version);
+      } else {
+        LOG_WARNING << "Failed to get bootloader version: " << std::get<0>(cur_version);
       }
-      LOG_INFO << "Current firmware version: " << sink;
-      if (sink != version) {
-        LOG_INFO << "Update firmware to version: " << version;
-        if (Utils::shell("fiovb_setenv bootupgrade_available 1", &sink) != 0) {
-          LOG_WARNING << "Failed to set bootupgrade_available";
-        }
+      if (std::get<0>(cur_version) != target_version) {
+        LOG_INFO << "Bootloader will be updated to version: " << target_version;
+        setEnvVar("bootupgrade_available", "1");
       }
     } break;
     default:
@@ -98,30 +80,51 @@ std::string BootloaderLite::getVersion(const std::string& deployment_dir, const 
 }
 
 bool BootloaderLite::isUpdateInProgress() const {
+  const auto ba_val{getEnvVar("bootupgrade_available")};
+  if (!std::get<1>(ba_val)) {
+    LOG_ERROR << "Failed to get `bootupgrade_available` value, assuming it is set to 0; err: " << std::get<0>(ba_val);
+    return false;
+  }
+  return std::get<0>(ba_val) == "1";
+}
+
+bool BootloaderLite::setEnvVar(const std::string& var_name, const std::string& var_val) const {
+  const std::unordered_map<RollbackMode, std::string> typeToCmd{
+      {RollbackMode::kUbootMasked, "fw_setenv"},
+      {RollbackMode::kFioVB, "fiovb_setenv"},
+  };
+  if (0 == typeToCmd.count(config_.rollback_mode)) {
+    LOG_DEBUG << "No command to set environment variable found for the given bootloader type: "
+              << config_.rollback_mode;
+    return false;
+  }
+  const auto cmd{boost::format{"%s %s %s"} % typeToCmd.at(config_.rollback_mode) % var_name % var_val};
+  std::string output;
+  if (Utils::shell(cmd.str(), &output) != 0) {
+    LOG_WARNING << "Failed to set the bootloader's environment variable" << var_name << "; err: " << output;
+    return false;
+  }
+  return true;
+}
+
+std::tuple<std::string, bool> BootloaderLite::getEnvVar(const std::string& var_name) const {
   const std::unordered_map<RollbackMode, std::string> typeToCmd{
       {RollbackMode::kUbootMasked, "fw_printenv -n"},
       {RollbackMode::kFioVB, "fiovb_printenv"},
   };
   if (0 == typeToCmd.count(config_.rollback_mode)) {
-    return false;
+    const auto er_msg{boost::format("No command to read environment variable found for `%s` bootloader type") %
+                      config_.rollback_mode};
+    return {er_msg.str(), false};
   }
-  int bootupgrade_available{readBootUpgradeAvailable(typeToCmd.at(config_.rollback_mode))};
-  return bootupgrade_available == 1;
-}
-
-int BootloaderLite::readBootUpgradeAvailable(const std::string& get_cmd) {
-  int bootupgrade_available{0};
-  std::string ba_str{"0"};
-
-  try {
-    if (Utils::shell(get_cmd + " bootupgrade_available", &ba_str) != 0) {
-      LOG_ERROR << "Failed to read bootupgrade_available, assume it is set to 0";
-    }
-    bootupgrade_available = std::stoi(ba_str);
-  } catch (const std::exception& exc) {
-    LOG_ERROR << "Failed to get `bootupgrade_available` value: " << exc.what() << "; assume it is set to 0";
+  const auto cmd{boost::format{"%s %s"} % typeToCmd.at(config_.rollback_mode) % var_name};
+  std::string output;
+  if (Utils::shell(cmd.str(), &output) != 0) {
+    const auto er_msg{boost::format("Failed to get a bootloader environment variable; cmd: %s, err: %s") % cmd %
+                      output};
+    return {er_msg.str(), false};
   }
-  return bootupgrade_available;
+  return {output, true};
 }
 
 }  // namespace bootloader
