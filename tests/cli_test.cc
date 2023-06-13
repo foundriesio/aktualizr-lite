85d0cd94e7f60c41bb3d43fb3982c9940f571fc4
Mike Sul <mike.sul@foundries.io>
2023-06-13 08:34:36 +0200

    tests: Add test to verify the TUF meta update failures
    - TUF meta update failure.
    - TUF target is not found in the successfully update TUF targets meta.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- tests/cli_test.cc
+++ tests/cli_test.cc
@@ -20,10 +20,14 @@ class CliClient : public AkliteTest {
     return std::make_shared<AkliteClient>(createLiteClient(initial_version));
   }
 
-  TufTarget createTufTarget() {
-    auto app01 = registry.addApp(fixtures::ComposeApp::create("app-01"));
-    std::vector<AppEngine::App> apps{app01};
-    return Target::toTufTarget(createTarget(&apps));
+  TufTarget createTufTarget(AppEngine::App* app = nullptr, const std::string& hwid = "") {
+    std::vector<AppEngine::App> apps;
+    if (!app) {
+      apps.emplace_back(registry.addApp(fixtures::ComposeApp::create("app-01")));
+    } else {
+      apps.push_back(*app);
+    }
+    return Target::toTufTarget(createTarget(&apps, hwid));
   }
 
   void reboot(std::shared_ptr<AkliteClient>& client, bool reset_bootupgrade_flag = true) {
@@ -34,6 +38,12 @@ class CliClient : public AkliteTest {
     }
     client = std::make_shared<AkliteClient>(createLiteClient(InitialVersion::kOff, app_shortlist_, false));
   }
+
+  void tweakConf(Config& conf) override { conf.uptane.repo_server = tuf_repo_server_; }
+
+ protected:
+  std::string tuf_repo_server_{device_gateway_.getTufRepoUri()};
+  std::string ostree_server_uri_{device_gateway_.getOsTreeUri()};
 };
 
 TEST_P(CliClient, FullUpdate) {
@@ -45,6 +55,29 @@ TEST_P(CliClient, FullUpdate) {
   ASSERT_TRUE(akclient->IsInstallationInProgress());
   ASSERT_EQ(akclient->GetPendingTarget(), target01);
   ASSERT_EQ(cli::CompleteInstall(*akclient), cli::StatusCode::Ok);
+}
+
+TEST_P(CliClient, TufMetaDownloadFailure) {
+  // make the TUF server URI invalid so the TUF metadata update fails
+  tuf_repo_server_ = device_gateway_.getTufRepoUri() + "/foobar";
+  auto akclient{createAkClient()};
+  const auto target01{createTufTarget()};
+
+  ASSERT_EQ(cli::Install(*akclient, target01.Version()), cli::StatusCode::TufMetaPullFailure);
+}
+
+TEST_P(CliClient, TufTargetNotFoundInvalidHardwareId) {
+  auto akclient{createAkClient()};
+  auto target01 = createTufTarget(nullptr, "foobar-hwid");
+
+  ASSERT_EQ(cli::Install(*akclient, target01.Version()), cli::StatusCode::TufTargetNotFound);
+}
+
+TEST_P(CliClient, TufTargetNotFoundInvalidVersion) {
+  auto akclient{createAkClient()};
+  auto target01 = createTufTarget(nullptr, "foobar-hwid");
+
+  ASSERT_EQ(cli::Install(*akclient, 100), cli::StatusCode::TufTargetNotFound);
 }
 
 INSTANTIATE_TEST_SUITE_P(MultiEngine, CliClient, ::testing::Values("RestorableAppEngine"));
