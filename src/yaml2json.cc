80e3e5d059c4f9e3f43632374ddd3261cdbd809f
Mike Sul <mike.sul@foundries.io>
2023-07-20 13:26:08 +0200

    yaml2json: Throw exception with meaningful message
    Detect each type of error separately and throw an exception with a
    message that meaningfully explains the error type.
    Add unit tests for each type of the error.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/yaml2json.cc
+++ src/yaml2json.cc
@@ -1,15 +1,26 @@
 #include "yaml2json.h"
+
+#include <filesystem>
+
 #include <logging/logging.h>
 #include "utilities/utils.h"
 
 Yaml2Json::Yaml2Json(const std::string& yaml) {
+  if (!std::filesystem::exists(yaml)) {
+    throw std::invalid_argument("The specified `yaml` file is not found: " + yaml);
+  }
   std::string cmd = "/usr/bin/fy-tool --mode json " + yaml;
   std::string data;
-  if (Utils::shell(cmd, &data, true) == EXIT_SUCCESS) {
+  // Parse the input yaml file
+  if (Utils::shell(cmd, &data, true) != EXIT_SUCCESS) {
+    throw std::invalid_argument("Failed to parse the input `yaml` file; path: " + yaml + ", err: " + data);
+  }
+  // Parse the resultant json representation of the input yaml file
+  try {
     std::istringstream sin(data);
     sin >> root_;
-  }
-  if (root_.empty()) {
-    throw std::runtime_error(cmd.c_str());
+  } catch (const std::exception& exc) {
+    throw std::invalid_argument("Failed to parse the json representation of the input `yaml` file; path: " + yaml +
+                                ", err: " + exc.what());
   }
 }
