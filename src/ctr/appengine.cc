ab0188fb48475724e803715848956e65dacfd9ba
Mike Sul <mike.sul@foundries.io>
2023-12-13 18:22:19 +0100

    composectl: Make clang format happy
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/ctr/appengine.cc
+++ src/ctr/appengine.cc
@@ -7,35 +7,38 @@
 #include "storage/stat.h"
 
 namespace ctr {
-  enum class ExitCode {
-    ExitCodeInsufficientSpace = 100
-  };
+enum class ExitCode { ExitCodeInsufficientSpace = 100 };
 
-
-  AppEngine::Result AppEngine::fetch(const App& app) {
+AppEngine::Result AppEngine::fetch(const App& app) {
   Result res{false};
-    try {
-      if (local_source_path_.empty()) {
-        exec(boost::format{"%s --store %s pull -p %s --storage-usage-watermark %d"} % composectl_cmd_ % storeRoot() % app.uri % storage_watermark_, "failed to pull compose app");
-      } else {
-        exec(boost::format{"%s --store %s pull -p %s -l %s --storage-usage-watermark %d"} % composectl_cmd_ % storeRoot() % app.uri % local_source_path_ % storage_watermark_, "failed to pull compose app");
-      }
-      res = true;
-    } catch (const ExecError& exc) {
-      if (exc.ExitCode == static_cast<int>(ExitCode::ExitCodeInsufficientSpace)) {
-        const auto usage_stat{Utils::parseJSON(exc.StdErr)};
-        auto usage_info{storageSpaceFunc()(usage_stat["path"].asString())};
-        res = {Result::ID::InsufficientSpace, exc.what(), usage_info.withRequired(usage_stat["required"].asUInt64())};
-      } else {
-        res = {false, exc.what()};
-      }
-    } catch (const std::exception& exc) {
+  try {
+    if (local_source_path_.empty()) {
+      exec(boost::format{"%s --store %s pull -p %s --storage-usage-watermark %d"} % composectl_cmd_ % storeRoot() %
+               app.uri % storage_watermark_,
+           "failed to pull compose app");
+    } else {
+      exec(boost::format{"%s --store %s pull -p %s -l %s --storage-usage-watermark %d"} % composectl_cmd_ %
+               storeRoot() % app.uri % local_source_path_ % storage_watermark_,
+           "failed to pull compose app");
+    }
+    res = true;
+  } catch (const ExecError& exc) {
+    if (exc.ExitCode == static_cast<int>(ExitCode::ExitCodeInsufficientSpace)) {
+      const auto usage_stat{Utils::parseJSON(exc.StdErr)};
+      auto usage_info{storageSpaceFunc()(usage_stat["path"].asString())};
+      res = {Result::ID::InsufficientSpace, exc.what(), usage_info.withRequired(usage_stat["required"].asUInt64())};
+    } else {
       res = {false, exc.what()};
     }
-    return res;
+  } catch (const std::exception& exc) {
+    res = {false, exc.what()};
   }
-  void AppEngine::installAppAndImages(const App& app) {
-    exec(boost::format{"%s --store %s --compose %s --host %s install %s"} % composectl_cmd_ % storeRoot() % installRoot() % dockerHost() % app.uri, "failed to installl compose app");
-  }
+  return res;
+}
+void AppEngine::installAppAndImages(const App& app) {
+  exec(boost::format{"%s --store %s --compose %s --host %s install %s"} % composectl_cmd_ % storeRoot() %
+           installRoot() % dockerHost() % app.uri,
+       "failed to installl compose app");
+}
 
-} // namespace ctr
+}  // namespace ctr
--- src/ctr/appengine.h
+++ src/ctr/appengine.h
@@ -7,33 +7,26 @@ namespace ctr {
 
 class AppEngine : public Docker::RestorableAppEngine {
  public:
-
-   AppEngine(
-      boost::filesystem::path store_root, boost::filesystem::path install_root, boost::filesystem::path docker_root,
-      Docker::RegistryClient::Ptr registry_client, Docker::DockerClient::Ptr docker_client,
-      std::string client = "/sbin/skopeo", std::string docker_host = "unix:///var/run/docker.sock",
-      std::string compose_cmd = "/usr/bin/docker-compose", std::string composectl_cmd = "/usr/bin/composectl", int storage_watermark = 80,
-      StorageSpaceFunc storage_space_func = RestorableAppEngine::GetDefStorageSpaceFunc(),
-      ClientImageSrcFunc client_image_src_func = nullptr,
-      bool create_containers_if_install = true, std::string local_source_path = ""):
-        Docker::RestorableAppEngine(store_root,
-                                    install_root,
-                                    docker_root,
-                                    registry_client,
-                                    docker_client,
-                                    client,
-                                    docker_host,
-                                    compose_cmd,
-                                    storage_space_func,
-                                    client_image_src_func,
-                                    create_containers_if_install,
-                                    !local_source_path.empty()),
-                                    composectl_cmd_{composectl_cmd}, storage_watermark_{storage_watermark}, local_source_path_{local_source_path} {}
-
+  AppEngine(boost::filesystem::path store_root, boost::filesystem::path install_root,
+            boost::filesystem::path docker_root, Docker::RegistryClient::Ptr registry_client,
+            Docker::DockerClient::Ptr docker_client, std::string client = "/sbin/skopeo",
+            std::string docker_host = "unix:///var/run/docker.sock",
+            std::string compose_cmd = "/usr/bin/docker-compose", std::string composectl_cmd = "/usr/bin/composectl",
+            int storage_watermark = 80,
+            StorageSpaceFunc storage_space_func = RestorableAppEngine::GetDefStorageSpaceFunc(),
+            ClientImageSrcFunc client_image_src_func = nullptr, bool create_containers_if_install = true,
+            std::string local_source_path = "")
+      : Docker::RestorableAppEngine(store_root, install_root, docker_root, registry_client, docker_client, client,
+                                    docker_host, compose_cmd, storage_space_func, client_image_src_func,
+                                    create_containers_if_install, !local_source_path.empty()),
+        composectl_cmd_{composectl_cmd},
+        storage_watermark_{storage_watermark},
+        local_source_path_{local_source_path} {}
 
   Result fetch(const App& app) override;
-  private:
-   void installAppAndImages(const App& app) override;
+
+ private:
+  void installAppAndImages(const App& app) override;
 
  private:
   const std::string composectl_cmd_;
@@ -44,4 +37,3 @@ class AppEngine : public Docker::RestorableAppEngine {
 }  // namespace ctr
 
 #endif  // AKTUALIZR_LITE_CTR_APP_ENGINE_H
-
