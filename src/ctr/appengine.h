eb54c8a50438373623380b3ea1983691528077a9
Mike Sul <mike.sul@foundries.io>
2023-12-14 12:09:33 +0100

    ctr: appengine: Make clang tidy happy
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/ctr/appengine.h
+++ src/ctr/appengine.h
@@ -15,11 +15,13 @@ class AppEngine : public Docker::RestorableAppEngine {
             int storage_watermark = 80,
             StorageSpaceFunc storage_space_func = RestorableAppEngine::GetDefStorageSpaceFunc(),
             ClientImageSrcFunc client_image_src_func = nullptr, bool create_containers_if_install = true,
-            std::string local_source_path = "")
-      : Docker::RestorableAppEngine(store_root, install_root, docker_root, registry_client, docker_client, client,
-                                    docker_host, compose_cmd, storage_space_func, client_image_src_func,
-                                    create_containers_if_install, !local_source_path.empty()),
-        composectl_cmd_{composectl_cmd},
+            const std::string& local_source_path = "")
+      : Docker::RestorableAppEngine(std::move(store_root), std::move(install_root), std::move(docker_root),
+                                    std::move(registry_client), std::move(docker_client), std::move(client),
+                                    std::move(docker_host), std::move(compose_cmd), std::move(storage_space_func),
+                                    std::move(client_image_src_func), create_containers_if_install,
+                                    !local_source_path.empty()),
+        composectl_cmd_{std::move(composectl_cmd)},
         storage_watermark_{storage_watermark},
         local_source_path_{local_source_path} {}
 
@@ -28,7 +30,6 @@ class AppEngine : public Docker::RestorableAppEngine {
  private:
   void installAppAndImages(const App& app) override;
 
- private:
   const std::string composectl_cmd_;
   const int storage_watermark_;
   const std::string local_source_path_;
