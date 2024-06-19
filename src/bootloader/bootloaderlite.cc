foundriesio/aktualizr-lite

commit fa9e0e543c054a56dab5f355d385313e1523842e

bootloader: add support for env variables
Add support for providing environment variables when calling
rollback-related utils like fw_setenv/fw_printenv/
fiovb_setenv/fiovb_printenv.

Provide by default FIO_OSTREE_TARGET_SYSROOT, which contains a path
to target ostree sysroot.

Signed-off-by: Igor Opaniuk <igor.opaniuk@foundries.io>

src/bootloader/bootloaderlite.cc
@@ -36,6 +36,9 @@ void BootloaderLite::installNotify(const Uptane::Target& target) const {
     case RollbackMode::kBootloaderNone:
     case RollbackMode::kUbootGeneric:
       break;
+    case RollbackMode::kFioEFI:
+          env_cmd_vars_ = boost::str(boost::format("%s=%s") % OstreeTargetPathVar %
+                                     getTargetDir(sysroot_->deployment_path(), target.sha256Hash()));
     case RollbackMode::kUbootMasked:
     case RollbackMode::kFioVB: {
       const auto target_version_val{getDeploymentVersion(target.sha256Hash())};
@@ -95,23 +98,17 @@ BootloaderLite::VersionStrRes BootloaderLite::getDeploymentVersion(const std::st
 std::string BootloaderLite::getVersion(const std::string& deployment_dir, const std::string& hash,
                                        const std::string& ver_file) {
   try {
-    std::string file;
-    for (auto& p : boost::filesystem::directory_iterator(deployment_dir)) {
-      std::string dir = p.path().string();
-      if (!boost::filesystem::is_directory(dir)) {
-        continue;
-      }
-      if (boost::algorithm::contains(dir, hash)) {
-        file = dir + ver_file;
-        break;
-      }
-    }
-    if (file.empty()) {
-      LOG_WARNING << "Target deployment hash was not found";
+    std::string target_dir = getTargetDir(deployment_dir, hash);
+
+    if (target_dir.empty()) {
+      LOG_WARNING << "Target's system root directory is not found for hash: " << hash <<
+                   " in: " << deployment_dir;
       return std::string();
     }
+
+    std::string file = target_dir + ver_file;
     if (!boost::filesystem::exists(file)) {
-      LOG_DEBUG << "Target's bootloader version file is not found, expected: " << file;
+      LOG_WARNING << "Target's bootloader version file is not found, expected: " << file;
       return std::string();
     }
 
@@ -123,6 +120,21 @@ std::string BootloaderLite::getVersion(const std::string& deployment_dir, const 
     LOG_ERROR << "Failed to get Target firmware version:  " << exc.what();
     return "";
   }
+}
+
+std::string BootloaderLite::getTargetDir(const std::string& deployment_dir,
+                                         const std::string& target_hash) {
+  for (auto& p : boost::filesystem::directory_iterator(deployment_dir)) {
+    std::string dir = p.path().string();
+    if (!boost::filesystem::is_directory(dir)) {
+      continue;
+    }
+    if (boost::algorithm::contains(dir, target_hash)) {
+      return dir;
+    }
+  }
+
+  return "";
 }
 
 bool BootloaderLite::isUpdateInProgress() const {
@@ -168,7 +180,7 @@ std::tuple<std::string, bool> BootloaderLite::setEnvVar(const std::string& var_n
         config_.rollback_mode};
     return {er_msg.str(), false};
   }
-  const auto cmd{boost::format{"%s %s %s"} % set_env_cmd_ % var_name % var_val};
+  const auto cmd{boost::format{"%s %s %s %s"} % env_cmd_vars_ % set_env_cmd_ % var_name % var_val};
   std::string output;
   if (Utils::shell(cmd.str(), &output) != 0) {
     const auto er_msg{boost::format("Failed to set a bootloader environment variable; cmd: %s, err: %s") % cmd %
@@ -185,7 +197,7 @@ std::tuple<std::string, bool> BootloaderLite::getEnvVar(const std::string& var_n
         config_.rollback_mode};
     return {er_msg.str(), false};
   }
-  const auto cmd{boost::format{"%s %s"} % get_env_cmd_ % var_name};
+  const auto cmd{boost::format{"%s %s %s"} % env_cmd_vars_ % get_env_cmd_ % var_name};
   std::string output;
   if (Utils::shell(cmd.str(), &output) != 0) {
     const auto er_msg{boost::format("Failed to get a bootloader environment variable; cmd: %s, err: %s") % cmd %

src/bootloader/bootloaderlite.h
@@ -35,6 +35,7 @@ class BootFwUpdateStatus {
 class BootloaderLite : public Bootloader, public BootFwUpdateStatus {
  public:
   static constexpr const char* const VersionFile{"/usr/lib/firmware/version.txt"};
+  static constexpr const char* const OstreeTargetPathVar{"FIO_OSTREE_TARGET_SYSROOT"};
 
   explicit BootloaderLite(BootloaderConfig config, INvStorage& storage, OSTree::Sysroot::Ptr sysroot);
 
@@ -57,10 +58,13 @@ class BootloaderLite : public Bootloader, public BootFwUpdateStatus {
 
   static VersionNumbRes verStrToNumber(const std::string& ver_str);
   static std::string extractVersionValue(const std::string& version_line);
+  static std::string getTargetDir(const std::string& deployment_dir,
+                                  const std::string& target_hash);
 
   OSTree::Sysroot::Ptr sysroot_;
   const std::string get_env_cmd_;
   const std::string set_env_cmd_;
+  mutable std::string env_cmd_vars_;
 };
 
 }  // namespace bootloader

commit df0a6bbcdd6750e910e4cf97c9285148cfc52e74

bootloader: fix format of recently added code
Commit: fa9e0e5 (bootloader: add support for env variables, 2024-06-06)
introduced a few code formatting errors, now corrected by running `make
format`.

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

src/bootloader/bootloaderlite.cc
@@ -37,8 +37,8 @@ void BootloaderLite::installNotify(const Uptane::Target& target) const {
     case RollbackMode::kUbootGeneric:
       break;
     case RollbackMode::kFioEFI:
-          env_cmd_vars_ = boost::str(boost::format("%s=%s") % OstreeTargetPathVar %
-                                     getTargetDir(sysroot_->deployment_path(), target.sha256Hash()));
+      env_cmd_vars_ = boost::str(boost::format("%s=%s") % OstreeTargetPathVar %
+                                 getTargetDir(sysroot_->deployment_path(), target.sha256Hash()));
     case RollbackMode::kUbootMasked:
     case RollbackMode::kFioVB: {
       const auto target_version_val{getDeploymentVersion(target.sha256Hash())};
@@ -101,8 +101,7 @@ std::string BootloaderLite::getVersion(const std::string& deployment_dir, const 
     std::string target_dir = getTargetDir(deployment_dir, hash);
 
     if (target_dir.empty()) {
-      LOG_WARNING << "Target's system root directory is not found for hash: " << hash <<
-                   " in: " << deployment_dir;
+      LOG_WARNING << "Target's system root directory is not found for hash: " << hash << " in: " << deployment_dir;
       return std::string();
     }
 
@@ -122,8 +121,7 @@ std::string BootloaderLite::getVersion(const std::string& deployment_dir, const 
   }
 }
 
-std::string BootloaderLite::getTargetDir(const std::string& deployment_dir,
-                                         const std::string& target_hash) {
+std::string BootloaderLite::getTargetDir(const std::string& deployment_dir, const std::string& target_hash) {
   for (auto& p : boost::filesystem::directory_iterator(deployment_dir)) {
     std::string dir = p.path().string();
     if (!boost::filesystem::is_directory(dir)) {

src/bootloader/bootloaderlite.h
@@ -58,8 +58,7 @@ class BootloaderLite : public Bootloader, public BootFwUpdateStatus {
 
   static VersionNumbRes verStrToNumber(const std::string& ver_str);
   static std::string extractVersionValue(const std::string& version_line);
-  static std::string getTargetDir(const std::string& deployment_dir,
-                                  const std::string& target_hash);
+  static std::string getTargetDir(const std::string& deployment_dir, const std::string& target_hash);
 
   OSTree::Sysroot::Ptr sysroot_;
   const std::string get_env_cmd_;