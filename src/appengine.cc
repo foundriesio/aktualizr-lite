2a9293256e4e7ad59d2e55036f67dacc7e85bd8f
Mike Sul <mike.sul@foundries.io>
2024-07-31 19:00:01 +0200

    appengine: Add required header to use std::find
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

--- src/appengine.cc
+++ src/appengine.cc
@@ -1,4 +1,5 @@
 #include "appengine.h"
+#include <algorithm>
 
 bool operator&(const AppEngine::Apps& apps, const AppEngine::App& app) {
   return apps.end() != std::find(apps.begin(), apps.end(), app);
