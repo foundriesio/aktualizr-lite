foundriesio/aktualizr-lite

commit b628f99e4d464e0c15f04abf192d184da09d9db4

makefile: Add check operation on make update
This allows the user of the test environment to use `make update`
directly instead of having to call `make check` before that.

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

Makefile
@@ -42,6 +42,7 @@ check:
 	${BIN} check
 
 update:
+	${BIN} check
 	@ostree config set core.mode bare-user
 	${BIN} pull
 	@ostree config set core.mode bare-user-only

commit c6a93bcc8b61e9459e45a53a3033bb11791be625

makefile: Fix make update command
The additional aktualizr-lite check command added in commit b628f99
(makefile: Add check operation on make update) introduced an issue
where the `make update` command stops before trying the aktualizr-lite
pull and install commands when there is an update to be performed, since
in that case, the check command returns an exit code != 0.

This commit ignores the return code of the check command, allowing the
`make update` command to always proceed to pull + install.

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

Makefile
@@ -42,7 +42,7 @@ check:
 	${BIN} check
 
 update:
-	${BIN} check
+	${BIN} check || true
 	@ostree config set core.mode bare-user
 	${BIN} pull
 	@ostree config set core.mode bare-user-only