foundriesio/aktualizr-lite

commit 605b974a9fc8fd521f721ebbca075bf2a82a80d9

api: extended: Select target with all apps when running an apps sync
Previous code was using the current target object, which is based on the
current running system, and may not contain all available apps.
This could lead to an issue where the apps sync update fails later on in
the online case, since equality between the latest available and
selected target is checked, and all apps must be included.

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

src/aklite_client_ext.cc
@@ -123,7 +123,7 @@ GetTargetToInstallResult AkliteClientExt::GetTargetToInstall(const CheckInResult
     auto apps_to_update = client_->appsToUpdate(Target::fromTufTarget(current));
     if (force_apps_sync || !apps_to_update.empty()) {
       // Force installation of apps
-      res.selected_target = current;
+      res.selected_target = checkin_res.SelectTarget(current.Version());
       LOG_INFO
           << "The specified Target is already installed, enforcing installation to make sure it's synced and running:"
           << res.selected_target.Name();

commit 4df310196306526d152f9ab3ef66e5c897d1a74c

api: extended: Fix apps sync when current target is not in TUF metadata
Commit 605b974 (api: extended: Select target with all apps when running
an apps sync) introduced a regression, where the current target is
required to be present in TUF metadata in order to an apps sync update
to be performed.
This change allows the target object derived from DB state to be used
instead of the one that was created from the TUF metadata when the later
is not available.
The fix implemented in 605b974 would not be required anymore since the
introduction of the commit previous to this one (api: Fix rollback to
target that is not in TUF metadata). But instead of just reverting it,
the current commit prioritizes the use of the target derived from TUF
metadata for consistency with the typical use scenario.

Signed-off-by: Andre Detsch <andre.detsch@foundries.io>

src/aklite_client_ext.cc
@@ -126,6 +126,11 @@ GetTargetToInstallResult AkliteClientExt::GetTargetToInstall(const CheckInResult
     if (force_apps_sync || !apps_to_update.empty()) {
       // Force installation of apps
       res.selected_target = checkin_res.SelectTarget(current.Version());
+      if (res.selected_target.IsUnknown()) {
+        LOG_DEBUG << "Unable to find current version " << current.Version()
+                  << " in TUF targets list. Using current target from DB instead";
+        res.selected_target = current;
+      }
       LOG_INFO
           << "The specified Target is already installed, enforcing installation to make sure it's synced and running:"
           << res.selected_target.Name();