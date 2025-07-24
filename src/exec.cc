a7da093479212599263937b21b09d6dfa0dba38c
Mike Sul <mike.sul@foundries.io>
2025-07-24 13:28:19 +0200

    exec: Make exec non-template function
    There is no reason to keep the "exec()" function as template functions
    since its implementation was moved away from the `boost::process`.
    It helps to decrease compile time.
    
    Signed-off-by: Mike Sul <mike.sul@foundries.io>

---
+++ src/exec.cc
@@ -0,0 +1,57 @@
+#include "exec.h"
+
+#include <boost/filesystem.hpp>
+#include <boost/format.hpp>
+
+#include "logging/logging.h"
+
+void exec(const std::string& cmd, const std::string& err_msg_prefix, const boost::filesystem::path& start_dir,
+          std::string* output) {
+  std::string command = "timeout 900 " + cmd + " 2>&1";
+  if (!start_dir.empty()) {
+    command = "cd " + start_dir.string() + " && " + command;
+  }
+
+  LOG_DEBUG << "Running: `" << command << "`";
+  FILE* pipe = popen(command.c_str(), "r");
+  if (!pipe) {
+    throw std::runtime_error("exec: popen() failed!");
+  }
+  std::string result;
+  try {
+    std::array<char, 128> buffer_array = {};
+    char* buffer = buffer_array.data();
+    while (std::fgets(buffer, sizeof(buffer_array), pipe) != nullptr) {
+      result += buffer;
+    }
+
+    if (output != nullptr) {
+      *output = result;
+    }
+  } catch (const std::exception& e) {
+    pclose(pipe);
+    throw std::runtime_error(std::string("exec: ") + e.what());
+  }
+
+  int status = pclose(pipe);
+  if (status == -1) {
+    throw std::runtime_error("exec: pclose() failed!");
+  }
+
+  int exit_code = WEXITSTATUS(status);
+
+  if (exit_code == 124) {
+    // `timeout` command return code indicating that a timeout has occured
+    throw std::runtime_error("Timeout occurred while waiting for a child process completion");
+  }
+  LOG_DEBUG << "Command exited with code " << exit_code;
+
+  if (exit_code != EXIT_SUCCESS) {
+    throw ExecError(err_msg_prefix, cmd, result, exit_code);
+  }
+}
+
+void exec(const boost::format& cmd, const std::string& err_msg, const boost::filesystem::path& start_dir,
+          std::string* output) {
+  exec(cmd.str(), err_msg, start_dir, output);
+}
--- src/exec.h
+++ src/exec.h
@@ -1,11 +1,8 @@
 #ifndef AKTUALIZR_LITE_EXEC_H_
 #define AKTUALIZR_LITE_EXEC_H_
 
-#include <boost/asio/io_context.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/format.hpp>
-
-#include "logging/logging.h"
 
 struct ExecError : std::runtime_error {
   ExecError(const std::string& msg_prefix, const std::string& cmd, const std::string& err_msg, int exit_code)
@@ -16,57 +13,10 @@ struct ExecError : std::runtime_error {
   const std::string StdErr;
 };
 
-template <typename... Args>
-static void exec(const std::string& cmd, const std::string& err_msg_prefix,
-                 const boost::filesystem::path& start_dir = "", std::string* output = nullptr) {
-  std::string command = "timeout 900 " + cmd + " 2>&1";
-  if (!start_dir.empty()) {
-    command = "cd " + start_dir.string() + " && " + command;
-  }
+void exec(const std::string& cmd, const std::string& err_msg_prefix, const boost::filesystem::path& start_dir = "",
+          std::string* output = nullptr);
 
-  LOG_DEBUG << "Running: `" << command << "`";
-  FILE* pipe = popen(command.c_str(), "r");
-  if (!pipe) {
-    throw std::runtime_error("exec: popen() failed!");
-  }
-  std::string result;
-  try {
-    std::array<char, 128> buffer_array = {};
-    char* buffer = buffer_array.data();
-    while (std::fgets(buffer, sizeof(buffer_array), pipe) != nullptr) {
-      result += buffer;
-    }
-
-    if (output != nullptr) {
-      *output = result;
-    }
-  } catch (const std::exception& e) {
-    pclose(pipe);
-    throw std::runtime_error(std::string("exec: ") + e.what());
-  }
-
-  int status = pclose(pipe);
-  if (status == -1) {
-    throw std::runtime_error("exec: pclose() failed!");
-  }
-
-  int exit_code = WEXITSTATUS(status);
-
-  if (exit_code == 124) {
-    // `timeout` command return code indicating that a timeout has occured
-    throw std::runtime_error("Timeout occurred while waiting for a child process completion");
-  }
-  LOG_DEBUG << "Command exited with code " << exit_code;
-
-  if (exit_code != EXIT_SUCCESS) {
-    throw ExecError(err_msg_prefix, cmd, result, exit_code);
-  }
-}
-
-template <typename... Args>
 void exec(const boost::format& cmd, const std::string& err_msg, const boost::filesystem::path& start_dir = "",
-          std::string* output = nullptr) {
-  exec(cmd.str(), err_msg, start_dir, output);
-}
+          std::string* output = nullptr);
 
 #endif  // AKTUALIZR_LITE_EXEC_H_
